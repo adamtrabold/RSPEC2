f0c8cd401136fdcd44050c105cc1e8a46fba2750
adamtrabold <adamtrabold@gmail.com>
2013-09-11 13:44:18 -0500

    Initial Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RSPEC2::Application.config.secret_token = '963e88ff6d9e4b7f3370810393e2817a53d5106242c0337679133106a835f4d2fd0fcfe91794b40365d5157742b61f7df51eebd8b261dd2105669d70d8295d56'
